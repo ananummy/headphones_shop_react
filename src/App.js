8e46c06894b74fa315de5146e3b250ddf0ee4d8c
Anastasia E <redfox.gazhil@gmail.com>
2023-12-02 22:38:02 +0300

    del some in app

--- src/App.js
+++ src/App.js
@@ -8,28 +8,14 @@ import Footer from './pages/Footer';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <BrowserRouter>
-          <Header />
-          <Routes>
-            <Route path={'/'} element={<MainPage />} />
-            <Route path={'/cart'} element={<CartPage />} />
-          </Routes>
-          <Footer />
-        </BrowserRouter>
-        {/* <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a> */}
-      </header>
+      <BrowserRouter>
+        <Header />
+        <Routes>
+          <Route path={'/'} element={<MainPage />} />
+          <Route path={'/cart'} element={<CartPage />} />
+        </Routes>
+        <Footer />
+      </BrowserRouter>
     </div>
   );
 }
